e185bb6c083ed622f6092faa7f57a4209d5b0194
eymin1259 <eymin1259@gmail.com>
2023-03-20 23:52:58 +0900

    rtk-query 회원가입 mutation 구현

--- ChatGPTPaintApp/src/pages/SignUp.tsx
+++ ChatGPTPaintApp/src/pages/SignUp.tsx
@@ -2,20 +2,28 @@ import React, {useCallback} from 'react';
 import SignUpPage from '../components/pages/SignUpPage';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../AppInner';
+import {useSignUpPostMutation} from '../api/authApi';
 
 type SignUpPageProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
-export type SignUpForm = {
+export interface SignUpForm {
   email: string;
   password: string;
   cfpassword: string;
-};
+}
 
 const SignUp = ({navigation}: SignUpPageProps) => {
-  const onSubmit = useCallback((data: SignUpForm) => {
-    console.log('SignUp');
-    console.log(data);
-  }, []);
+  const [singUp, {isLoading, data: uid, isError, error}] =
+    useSignUpPostMutation();
+
+  const onSubmit = useCallback(
+    (form: SignUpForm) => {
+      console.log('SignUp');
+      console.log(form);
+      singUp({email: form.email, password: form.password});
+    },
+    [singUp],
+  );
 
   const gotoSignIn = useCallback(() => {
     navigation.pop();
