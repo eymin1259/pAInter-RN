ac83ac5e33784ebf69791f8082a19bbb58e57f91
eymin1259 <eymin1259@gmail.com>
2023-03-27 13:07:19 +0900

    코드푸쉬 설정

--- ChatGPTPaintApp/App.tsx
+++ ChatGPTPaintApp/App.tsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
 import 'react-native-gesture-handler';
+import CodePush, {CodePushOptions} from 'react-native-code-push';
 
 function App() {
   return (
@@ -15,4 +16,10 @@ function App() {
   );
 }
 
-export default App;
+const codePushOptions: CodePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  installMode: CodePush.InstallMode.IMMEDIATE,
+  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+};
+
+export default CodePush(codePushOptions)(App);
