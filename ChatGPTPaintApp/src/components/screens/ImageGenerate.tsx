7581970438063ab8637c421f854b54c891075ebd
eymin1259 <eymin1259@gmail.com>
2023-03-27 10:06:03 +0900

    PromptHeader 컴포넌트 분리

--- ChatGPTPaintApp/src/components/screens/ImageGenerate.tsx
+++ ChatGPTPaintApp/src/components/screens/ImageGenerate.tsx
@@ -1,18 +1,12 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {
-  ActivityIndicator,
-  Alert,
-  LayoutChangeEvent,
-  Text,
-  Keyboard,
-} from 'react-native';
+import {Alert, LayoutChangeEvent, Keyboard} from 'react-native';
 import styled from '@emotion/native';
 import PurpleButton from '../common/PurpleButton';
 import PreviewImage from '../common/PreviewImage';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useGenerateImageMutation} from '../../hooks/api/useGenerateImage';
 import ViewShot from 'react-native-view-shot';
 import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import PromptHeader from '../common/PromptHeader';
 
 const ImageGenerate = () => {
   const [parentLayout, setParentLayout] = useState({
@@ -33,6 +27,13 @@ const ImageGenerate = () => {
     });
   };
 
+  useEffect(() => {
+    if (isError) {
+      const errorMessage = error as string;
+      Alert.alert('alert', errorMessage);
+    }
+  }, [isError, error]);
+
   const onClickGenerate = useCallback(() => {
     if (!prompt) {
       return;
@@ -40,13 +41,6 @@ const ImageGenerate = () => {
     Keyboard.dismiss();
     generateImage(prompt);
   }, [prompt]);
-
-  useEffect(() => {
-    if (isError) {
-      const errorMessage = error as string;
-      Alert.alert('alert', errorMessage);
-    }
-  }, [isError, error]);
 
   const onClickSaveImage = useCallback(async () => {
     if (saveLoading || imageRef.current === null) {
@@ -63,23 +57,13 @@ const ImageGenerate = () => {
 
   return (
     <ImageGenerateLayout onLayout={onLayout}>
-      <SearchHeader>
-        <GeneratePrompt
-          width={parentLayout.width * 0.65}
-          multiline={false}
-          placeholder="Describe the image"
-          onChangeText={e => setPropmt(e)}
-        />
-        <SearchButton disabled={isLoading} onPress={onClickGenerate}>
-          <Text>
-            {isLoading ? (
-              <ActivityIndicator color="white" />
-            ) : (
-              <Ionicons name="brush" size={20} color="white" />
-            )}
-          </Text>
-        </SearchButton>
-      </SearchHeader>
+      <PromptHeader
+        width={parentLayout.width * 0.9}
+        placeholder="Describe the image"
+        onChangeText={e => setPropmt(e)}
+        isLoading={isLoading}
+        onPress={onClickGenerate}
+      />
       <ViewShot
         ref={imageRef}
         options={{
@@ -111,39 +95,6 @@ const ImageGenerateLayout = styled.View`
   justify-content: flex-start;
   align-items: center;
   background-color: white;
-`;
-
-interface GeneratePromptProps {
-  width: number;
-}
-
-const GeneratePrompt = styled.TextInput`
-  width: ${(props: GeneratePromptProps) => {
-    return `${props.width}px`;
-  }};
-  height: 50px;
-  border: 1px solid lightgray;
-  border-radius: 10px;
-  padding: 0 10px;
-`;
-
-const SearchButton = styled.TouchableOpacity`
-  justify-content: center;
-  align-items: center;
-  width: 15%;
-  height: 48px;
-  background-color: purple;
-  border-radius: 10px;
-  margin-left: 10px;
-`;
-
-const SearchHeader = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 50px;
-  margin-top: 20px;
 `;
 
 export default ImageGenerate;
