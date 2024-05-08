d2d4f240f8eccfadb468a873cd229094b7c25610
eymin1259 <eymin1259@gmail.com>
2024-05-08 10:00:00 +0900

    로그인, 회원가입 react hook form 적용

--- ChatGPTPaintApp/src/components/pages/SignUpPage.tsx
+++ ChatGPTPaintApp/src/components/pages/SignUpPage.tsx
@@ -6,23 +6,87 @@ import AuthButton from '../auth/AuthButton';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview';
 import {Platform} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
+import {SignUpForm} from '../../pages/SignUp';
 
 interface ISignUpPageProps {
   gotoSignIn: () => void;
+  onSubmit: (data: SignUpForm) => void;
 }
 
-const SignUpPage = (props: ISignUpPageProps) => {
+const SignUpPage = ({gotoSignIn, onSubmit}: ISignUpPageProps) => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      cfpassword: '',
+    },
+  });
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.OutterContainer}>
         <KeyboardAwareScrollView>
           <View style={styles.InnerContainer}>
             <AuthTitle>SignUp</AuthTitle>
-            <EmailInput />
-            <PasswordInput />
-            <PasswordInput />
-            <AuthButton>Confirm</AuthButton>
-            <AuthButton onPress={props.gotoSignIn}>SignIn</AuthButton>
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: {value: true, message: "email can't be blank"},
+                pattern: {
+                  value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                  message: 'invalid email address',
+                },
+              }}
+              render={({field: {onChange}}) => (
+                <EmailInput
+                  onChangeText={onChange}
+                  error={errors.email?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              rules={{
+                required: {value: true, message: "password can't be blank"},
+                minLength: {
+                  value: 4,
+                  message: 'minimum is 4 characters',
+                },
+              }}
+              render={({field: {onChange}}) => (
+                <PasswordInput
+                  onChangeText={onChange}
+                  error={errors.password?.message}
+                  placeholder="password"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="cfpassword"
+              rules={{
+                validate: (val, formVal) => {
+                  return val === formVal.password;
+                },
+              }}
+              render={({field: {onChange}}) => (
+                <PasswordInput
+                  onChangeText={onChange}
+                  error={
+                    errors.cfpassword === undefined ? '' : 'password mismatch'
+                  }
+                  placeholder="confirm password"
+                />
+              )}
+            />
+            <AuthButton onPress={handleSubmit(onSubmit)}>Confirm</AuthButton>
+            <AuthButton onPress={gotoSignIn}>SignIn</AuthButton>
           </View>
         </KeyboardAwareScrollView>
       </View>
