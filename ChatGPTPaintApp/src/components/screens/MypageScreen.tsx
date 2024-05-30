0f52624996940693fa5d92a8fd561add1afca87e
eymin1259 <eymin1259@gmail.com>
2024-05-30 10:00:00 +0900

    회원탈퇴 모달 연결

--- ChatGPTPaintApp/src/components/screens/MypageScreen.tsx
+++ ChatGPTPaintApp/src/components/screens/MypageScreen.tsx
@@ -1,54 +1,27 @@
-import React, {useCallback, useState} from 'react';
-import {View} from 'react-native';
-import {useAppDispatch} from '../../store';
-import {removeUserFromStorage} from '../../thunks/encryptedStorageThunk';
-import Dialog from 'react-native-dialog';
-import auth from '@react-native-firebase/auth';
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../store/reducer';
 import TextBoxButton from '../common/TextBoxButton';
 
-const MyPageScreen = () => {
-  const dispatch = useAppDispatch();
-  const [visible, setVisible] = useState(false);
-  const [inputPassword, setInputPassword] = useState('');
+interface IMyPageScreenProps {
+  handleSignOut: () => void;
+  hadleDeleteAccount: () => void;
+}
 
-  const handleSignOut = useCallback(async () => {
-    await auth().signOut();
-    dispatch(removeUserFromStorage());
-  }, [dispatch]);
-
-  const handleCancel = () => {
-    setInputPassword('');
-    setVisible(false);
-  };
-
-  const handleDelete = () => {
-    console.log(inputPassword);
-    setInputPassword('');
-    setVisible(false);
-  };
+const MyPageScreen = ({
+  handleSignOut,
+  hadleDeleteAccount,
+}: IMyPageScreenProps) => {
+  const userEmail = useSelector((state: RootState) => state.user.email);
 
   return (
-    <View>
-      <TextBoxButton>email</TextBoxButton>
+    <>
+      <TextBoxButton>{userEmail}</TextBoxButton>
       <TextBoxButton onPress={handleSignOut}>sign out</TextBoxButton>
-      <TextBoxButton onPress={() => setVisible(true)} textColor="red">
+      <TextBoxButton onPress={hadleDeleteAccount} textColor="red">
         delete account
       </TextBoxButton>
-      <Dialog.Container visible={visible}>
-        <Dialog.Title>Account delete</Dialog.Title>
-        <Dialog.Description>
-          Do you want to delete this account? You cannot undo this action.
-        </Dialog.Description>
-        <Dialog.Input
-          placeholder="password"
-          onChangeText={e => {
-            setInputPassword(e);
-          }}
-        />
-        <Dialog.Button label="Cancel" onPress={handleCancel} />
-        <Dialog.Button label="Delete" onPress={handleDelete} />
-      </Dialog.Container>
-    </View>
+    </>
   );
 };
 
--- ChatGPTPaintApp/src/screens/Mypage.tsx
+++ ChatGPTPaintApp/src/screens/Mypage.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import MyPageScreen from '../components/screens/MyPageScreen';
 import {useAppDispatch} from '../store';
+import auth from '@react-native-firebase/auth';
+import {removeUserFromStorage} from '../thunks/encryptedStorageThunk';
+import InputDialog from '../components/common/InputDialog';
+import useDialog from '../hooks/useDialog';
+
 const MyPage = () => {
   const dispatch = useAppDispatch();
-  return <MyPageScreen />;
+  const {openDialog} = useDialog();
+
+  const handleSignOut = useCallback(async () => {
+    await auth().signOut();
+    dispatch(removeUserFromStorage());
+  }, [dispatch]);
+
+  const hadleDeleteAccount = async () => {
+    const payload = await openDialog({
+      title: 'Alert',
+      message: 'Do you want to delete this account?',
+    });
+    if (payload.result) {
+      console.log(`re-auth ${payload.dialogInput}`);
+    }
+  };
+
+  return (
+    <>
+      <MyPageScreen
+        handleSignOut={handleSignOut}
+        hadleDeleteAccount={hadleDeleteAccount}
+      />
+      <InputDialog
+        inputPlaceholder="password"
+        cancelBtnLabel="cancel"
+        confirmBtnLable="delete"
+      />
+    </>
+  );
 };
 
 export default MyPage;
