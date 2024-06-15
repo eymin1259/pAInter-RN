de29004a728da5a6b8da1be26f0ff07462c6aedc
eymin1259 <eymin1259@gmail.com>
2024-06-15 10:00:00 +0900

    타이틀 컴포넌트 ui 수정

--- ChatGPTPaintApp/src/components/common/Title.tsx
+++ ChatGPTPaintApp/src/components/common/Title.tsx
@@ -2,13 +2,31 @@ import React, {ReactNode} from 'react';
 import styled from '@emotion/native';
 
 interface ITitleProps {
-  children: ReactNode;
+  children?: ReactNode;
   textColor?: string;
   fontSize?: string;
+  paddingTop?: string;
+  paddingBottom?: string;
+  paddingLeft?: string;
+  paddingRight?: string;
 }
-const Title = ({children, textColor, fontSize}: ITitleProps) => {
+const Title = ({
+  children,
+  textColor,
+  fontSize,
+  paddingTop,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+}: ITitleProps) => {
   return (
-    <TitleText fontSize={fontSize} textColor={textColor}>
+    <TitleText
+      fontSize={fontSize}
+      textColor={textColor}
+      paddingTop={paddingTop}
+      paddingBottom={paddingBottom}
+      paddingLeft={paddingLeft}
+      paddingRight={paddingRight}>
       {children}
     </TitleText>
   );
@@ -17,6 +35,10 @@ const Title = ({children, textColor, fontSize}: ITitleProps) => {
 interface TitleTextProps {
   textColor?: string;
   fontSize?: string;
+  paddingTop?: string;
+  paddingBottom?: string;
+  paddingLeft?: string;
+  paddingRight?: string;
 }
 
 const TitleText = styled.Text`
@@ -24,6 +46,14 @@ const TitleText = styled.Text`
     props.fontSize ? props.fontSize : '30px'};
   color: ${(props: TitleTextProps) =>
     props.textColor ? props.textColor : 'purple'};
+  padding-top: ${(props: TitleTextProps) =>
+    props.paddingTop ? props.paddingTop : '0px'};
+  padding-bottom: ${(props: TitleTextProps) =>
+    props.paddingBottom ? props.paddingBottom : '0px'};
+  padding-left: ${(props: TitleTextProps) =>
+    props.paddingLeft ? props.paddingLeft : '0px'};
+  padding-right: ${(props: TitleTextProps) =>
+    props.paddingRight ? props.paddingRight : '0px'};
   font-weight: bold;
 `;
 
