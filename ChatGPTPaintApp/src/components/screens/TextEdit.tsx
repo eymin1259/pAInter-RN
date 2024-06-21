a2cfe2643cf4f979426586bc2d81ded85752abc6
eymin1259 <eymin1259@gmail.com>
2024-06-21 10:00:00 +0900

    텍스트 업로드 스크린 ui 개발

--- ChatGPTPaintApp/src/components/screens/TextEdit.tsx
+++ ChatGPTPaintApp/src/components/screens/TextEdit.tsx
@@ -1,22 +1,62 @@
-import React from 'react';
-import {Pressable, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {LayoutChangeEvent} from 'react-native';
 import {TextEditStackParamList} from '../../screens/TextEditScreen';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import styled from '@emotion/native';
+import ResourceUploadHeader from '../common/ResourceUploadHeader';
+import PurpleButton from '../common/PurpleButton';
 
 type TextEditProps = NativeStackScreenProps<TextEditStackParamList, 'TextEdit'>;
 
 const TextEdit = ({navigation}: TextEditProps) => {
+  const [parentHeight, setParentHeight] = useState(0);
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const {height} = event.nativeEvent.layout;
+    setParentHeight(height);
+  };
+
   return (
-    <View>
-      <Text>text edit</Text>
-      <Pressable
+    <TextEditLayout onLayout={onLayout}>
+      <TextEditContent height={parentHeight - 80}>
+        <ResourceUploadHeader
+          titleColor="black"
+          fontSize="25px"
+          paddingTop="20px"
+          paddingBottom="10px">
+          Upload Text
+        </ResourceUploadHeader>
+      </TextEditContent>
+      <PurpleButton
+        fontSize="20px"
         onPress={() => {
           navigation.navigate('TextEditResult');
         }}>
-        <Text>go to result</Text>
-      </Pressable>
-    </View>
+        Edit
+      </PurpleButton>
+    </TextEditLayout>
   );
 };
+
+const TextEditLayout = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: white;
+`;
+
+interface TextEditContentProps {
+  height: number;
+}
+
+const TextEditContent = styled.View`
+  width: 100%;
+  height: ${(props: TextEditContentProps) => {
+    return `${props.height}px`;
+  }};
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 export default TextEdit;
