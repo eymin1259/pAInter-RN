c2fe0a35f95dd1d925fad0e8d0be1cbea7a17a31
eymin1259 <eymin1259@gmail.com>
2024-06-23 10:00:00 +0900

    photoSlice 개발

--- ChatGPTPaintApp/src/store/reducer.ts
+++ ChatGPTPaintApp/src/store/reducer.ts
@@ -1,13 +1,14 @@
 import {combineReducers} from 'redux';
 import {firebaseAuthApi} from '../hooks/api/authApi';
 import dialogSlice from '../slices/dialogSlice';
-
+import photoSlice from '../slices/photoSlice';
 import userSlice from '../slices/userSlice';
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
   [firebaseAuthApi.reducerPath]: firebaseAuthApi.reducer,
   dialog: dialogSlice.reducer,
+  photo: photoSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
