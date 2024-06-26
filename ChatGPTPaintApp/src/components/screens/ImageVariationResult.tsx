7658cf9466070a52e78c292c7526ef0e32f459ca
eymin1259 <eymin1259@gmail.com>
2024-06-26 10:00:00 +0900

    ImageVariationResult 스크린 UI 개발

--- ChatGPTPaintApp/src/components/screens/ImageVariationResult.tsx
+++ ChatGPTPaintApp/src/components/screens/ImageVariationResult.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {LayoutChangeEvent} from 'react-native';
+import styled from '@emotion/native';
+import PreviewImage from '../common/PreviewImage';
+import PurpleButton from '../common/PurpleButton';
 
 const ImageVariationResult = () => {
+  const [parentLayout, setParentLayout] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const {height, width} = event.nativeEvent.layout;
+    setParentLayout({
+      width,
+      height,
+    });
+  };
   return (
-    <>
-      <Text>Image Variation Result</Text>
-    </>
+    <ImageVariationResultLayout onLayout={onLayout}>
+      <VariationResultContent height={parentLayout.height - 100}>
+        <PreviewImage
+          imageSize={parentLayout.width * 0.8}
+          isLoading={true}
+          imageUri=""
+        />
+      </VariationResultContent>
+      <PurpleButton
+        fontSize="20px"
+        isLoading={true}
+        onPress={() => {
+          console.log('save photo ');
+        }}>
+        save !
+      </PurpleButton>
+    </ImageVariationResultLayout>
   );
 };
+
+const ImageVariationResultLayout = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+interface VariationResultContentProps {
+  height: number;
+}
+
+const VariationResultContent = styled.View`
+  width: 100%;
+  height: ${(props: VariationResultContentProps) => {
+    return `${props.height}px`;
+  }};
+  justify-content: center;
+  align-items: center;
+`;
 
 export default ImageVariationResult;
